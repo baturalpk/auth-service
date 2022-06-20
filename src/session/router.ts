1f5e1c22c2b96521a3690b56d6238b8ebe9af203
baturalpk <baturalp.kiziltan@outlook.com>
2022-06-20 19:25:51 +0300

    (session) change method for "destroy" to POST

--- src/session/router.ts
+++ src/session/router.ts
@@ -6,6 +6,6 @@ const router = Router()
 
 router.post('/new', VerifyIdentity, handlers.NewSession)
 router.post('/validate', handlers.ValidateSession)
-router.delete('/destroy', handlers.DestroySession)
+router.post('/destroy', handlers.DestroySession);
 
 export default router;
